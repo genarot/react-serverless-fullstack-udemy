04a17fd4e12a447c80317a4856277a57dd5bc9f3
Tinoco, Genaro <gtinoco@agssa.net>
2021-01-06 16:27:55 -0600

    Styled game page

--- src/pages/Game.tsx
+++ src/pages/Game.tsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
+import {
+  StyledCharacter,
+  StyledGame,
+  StyledScore,
+  StyledTimer,
+} from "../styled/Game";
+import { Strong } from "../styled/Random";
 
 export default function Game() {
-    return (
-        <div>
-            <h1>Game</h1>
-        </div>
-    )
+  return (
+    <StyledGame>
+      <StyledScore>Score:0</StyledScore>
+      <StyledCharacter>A</StyledCharacter>
+      <StyledTimer>
+        Time: <Strong>00: 000</Strong>
+      </StyledTimer>
+    </StyledGame>
+  );
 }
