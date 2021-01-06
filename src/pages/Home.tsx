dbd078b4dd5fd3bca1676c21a31e8aa22e8d9a56
Tinoco, Genaro <gtinoco@agssa.net>
2021-01-06 15:45:52 -0600

    Styled the Home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,14 @@
-import React from 'react'
-
+import React from "react";
+import CTA from "../styled/CTA";
+import { Accent, StyledTitle } from "../styled/Random";
 
 export default function Home() {
-    return (
-        <div>
-            <h1>Home</h1>
-        </div>
-    )
+  return (
+    <div>
+      <StyledTitle>Ready to type?</StyledTitle>
+      <CTA to="/game">
+        Click or type '<Accent>s</Accent>' to start playing!
+      </CTA>
+    </div>
+  );
 }
