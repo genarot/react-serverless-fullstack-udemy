77f9da43f5b377689e44a9496953bf54e5dd32a7
Tinoco, Genaro <gtinoco@quoininc.com>
2021-01-08 16:59:35 -0600

    Saving score

--- src/pages/GameOver.tsx
+++ src/pages/GameOver.tsx
@@ -1,11 +1,33 @@
+import { useEffect, useState } from "react";
 import { useScore } from "../contexts/ScoreContexts";
 import { StyledLink } from "../styled/StyledNavbar";
 
 const GameOver = ({ history }: any) => {
   const [score] = useScore();
+  const [scoreMessage, setscoreMessage] = useState("");
   if (score === -1) {
     history.push("/");
   }
+
+  useEffect(() => {
+    const saveHighScore = async () => {
+      if (score > 0) {
+        try {
+          const options = {
+            method: "POST",
+            body: JSON.stringify({ name: "James", score }),
+          };
+          const res = await fetch(".netlify/functions/saveHighScore", options);
+          const data = await res.json();
+          console.log(data);
+        } catch (err) {
+          console.error(err);
+        }
+      }
+    };
+    saveHighScore();
+  }, []);
+
   return (
     <div>
       <h1>GameOver</h1>
--- src/pages/HighScores.tsx
+++ src/pages/HighScores.tsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import { useEffect, useState } from "react";
+import { ScoreLI, ScoresList } from "../styled/ScoresList";
+
+interface IScore {
+  id: string;
+  fields: {
+    name: string;
+    score: number;
+  };
+}
 
 export default function HighScores() {
-    return (
-        <div>
-            <h1>HighScores</h1>
-        </div>
-    )
+  const [highScores, setHighScores] = useState<IScore[]>([]);
+  useEffect(() => {
+    fetchHighScores();
+  }, []);
+
+  const fetchHighScores = async () => {
+    try {
+      const response = await fetch("/.netlify/functions/getHighScores");
+      const scores = await response.json();
+      console.log("getting ", scores);
+      setHighScores(scores);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  return (
+    <div>
+      <h1>HighScores</h1>
+      <ScoresList>
+        {highScores.map((score) => (
+          <ScoreLI key={score.id}>
+            {score.fields.name} - {score.fields.score}
+          </ScoreLI>
+        ))}
+      </ScoresList>
+    </div>
+  );
 }
