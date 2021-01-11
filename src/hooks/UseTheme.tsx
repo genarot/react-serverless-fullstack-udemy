06751fcfd82743e1f133e1ef8c85259800ae0ce5
Tinoco, Genaro <gtinoco@agssa.net>
2021-01-11 15:18:15 -0600

    Persisting theme preference in local storage

--- src/hooks/UseTheme.tsx
+++ src/hooks/UseTheme.tsx
@@ -1,15 +1,22 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function UseTheme() {
   const [theme, setTheme] = useState("dark");
 
+  useEffect(() => {
+    const localStorageTheme = window.localStorage.getItem("theme");
+    setTheme(localStorageTheme || "light");
+  }, []);
+
   const toggleTheme = () => {
     if (theme === "light") {
-      console.log("light");
+      console.log("dark");
       setTheme("dark");
+      window.localStorage.setItem("theme", "dark");
     } else {
       setTheme("light");
-      console.log("dark");
+      console.log("light");
+      window.localStorage.setItem("theme", "light");
     }
   };
 
