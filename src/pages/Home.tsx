4a8ea64543be044fd7b456e5c03ae84c8e422f0f
Tinoco, Genaro <gtinoco@agssa.net>
2021-01-11 15:45:30 -0600

    Updated .env example

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,10 @@
 import { useAuth0 } from "@auth0/auth0-react";
-import React, { useEffect } from "react";
+import React from "react";
 import CTA from "../styled/CTA";
 import { Accent, StyledTitle } from "../styled/Random";
 
 export default function Home() {
-  const { user, isAuthenticated, getAccessTokenSilently } = useAuth0();
+  const { user } = useAuth0();
 
   // useEffect(() => {
   //   const getToken = async () => {
