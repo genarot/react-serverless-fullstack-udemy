ab084cd12a52e6ef2e44df33370cd2837df15efb
Tinoco, Genaro <gtinoco@quoininc.com>
2021-01-11 10:49:47 -0600

    Saving high score

--- src/pages/GameOver.tsx
+++ src/pages/GameOver.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { useScore } from "../contexts/ScoreContexts";
+import { StyledCharacter } from "../styled/Game";
 import { StyledLink } from "../styled/StyledNavbar";
 
 const GameOver = ({ history }: any) => {
@@ -19,6 +20,11 @@ const GameOver = ({ history }: any) => {
           };
           const res = await fetch(".netlify/functions/saveHighScore", options);
           const data = await res.json();
+          if (data.id) {
+            setscoreMessage("Congrats! you got a high score!");
+          } else {
+            setscoreMessage("Sorry, not a high score keep trying!!");
+          }
           console.log(data);
         } catch (err) {
           console.error(err);
@@ -31,6 +37,8 @@ const GameOver = ({ history }: any) => {
   return (
     <div>
       <h1>GameOver</h1>
+      <StyledCharacter>{score}</StyledCharacter>
+      <p>{scoreMessage}</p>
       <StyledLink to="/">Go Home</StyledLink>
       <StyledLink to="/game">Play again?</StyledLink>
     </div>
